b26093e740472becb880063fb72dd7122352abfd
Thiago Gomes <thiagogomesabreu01@gmail.com>
2024-06-11 19:41:22 -0300

    Mudando o curso

--- js/data.js
+++ js/data.js
@@ -2,25 +2,25 @@ const projectsList = [
     {
         name: "PLAYER DE MÚSICA",
         desc: "Uma página com um player de músicas, com todos os controles desenvolvidos por mim.",
-        href: "https://github.com/thgomes1/music-player",
+        href: "https://github.com/thgomxs/music-player",
         img: "./assets/p1.png",
     },
     {
         name: "CHURRASCÔMETRO",
         desc: "Uma página para calcular a quantidade de carnes e bebidas em um churrasco.",
-        href: "https://github.com/thgomes1/churrascometro",
+        href: "https://github.com/thgomxs/churrascometro",
         img: "./assets/p2.png",
     },
     {
         name: "LISTA DE TAREFAS",
         desc: "Uma página para adicionar tarefas e guardar os dados em local storage, salvando os mesmos.",
-        href: "https://github.com/thgomes1/to-do-list",
+        href: "https://github.com/thgomxs/to-do-list",
         img: "./assets/p3.png",
     },
     {
         name: "SORTEADOR",
         desc: "Uma página que sorteia nomes aleatoriamente.",
-        href: "https://github.com/thgomes1/sorteador-de-nomes",
+        href: "https://github.com/thgomxs/sorteador-de-nomes",
         img: "./assets/p7.png",
     },
 ];
